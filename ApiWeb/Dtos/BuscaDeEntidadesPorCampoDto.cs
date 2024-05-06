c9a61b346dd72be598cbbcd2bf10d18f2e76470b
Marlon Couto <ozair.couto@trieduc.com.br>
2024-05-06 00:05:40 -0300

    Atualiza classes

---
+++ ApiWeb/Dtos/BuscaDeEntidadesPorCampoDto.cs
@@ -0,0 +1,10 @@
+﻿using ApiWeb.Helpers;
+
+namespace ApiWeb.Dtos;
+
+public class BuscaDeEntidadesPorCampoDto
+{
+    public string Campo { get; set; } = string.Empty;
+    public object Valor { get; set; } = null!;
+    public Paginacao Paginacao { get; set; } = null!;
+}
--- ApiWeb/Helpers/RespostaDaApi.cs
+++ ApiWeb/Helpers/RespostaDaApi.cs
@@ -12,3 +12,15 @@ public class RespostaDaApi<T>
     public string Mensagem { get; set; } = string.Empty;
     public T? Dados { get; set; }
 }
+
+public class RespostaDaApiPaginada<T> : RespostaDaApi<T>
+{
+    public Paginacao Paginacao { get; set; } = null!;
+}
+
+public class Paginacao
+{
+    public int Pagina { get; set; }
+    public int QuantidadePorPagina { get; set; }
+    public int? Total { get; set; }
+}
